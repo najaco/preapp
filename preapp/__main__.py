5bd04a60ea29ddb2f20ecd826b0de067931be17a
Stephen Davis <davi1138@purdue.edu>
2020-05-10 16:16:44 -0400

    (nox): fixed linting errors

--- preapp/__main__.py
+++ preapp/__main__.py
@@ -6,12 +6,9 @@ from preapp import Node
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="tool for setting up software projects"
-    )
-    parser.add_argument("-p", "--preset", action='store', type=str)
+    parser = argparse.ArgumentParser(description="tool for setting up software projects")
+    parser.add_argument("-p", "--preset", action="store", type=str)
     args = parser.parse_args()
     root_node: RootNode = RootNode(args.preset)
     Node.register(root_node)
     root_node.process()
-    
--- preapp/nodes/root_node.py
+++ preapp/nodes/root_node.py
@@ -2,6 +2,7 @@ from .. import Node
 import json
 from typing import Dict, Any
 
+
 class RootNode(Node):
     """ The root node of the preapp application, All required entry point nodes are defined here """
 
@@ -13,10 +14,9 @@ class RootNode(Node):
 
     def pre_process(self):
         if self.preset != None:
-            config_fp: TextIOWrapper = open(self.preset, 'r')
+            config_fp: TextIOWrapper = open(self.preset, "r")
             config_json: Dict[str, Any] = json.load(config_fp)
             config_fp.close()
 
             for key, value in config_json.items():
                 Node._full_response[key] = value
-
