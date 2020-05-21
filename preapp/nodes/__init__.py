04de4fd2c62bcfb889c91b1c5db2e7b4cb97fc96
Stephen Davis <davi1138@purdue.edu>
2020-05-21 18:44:17 -0400

    (feature): added python node

--- preapp/nodes/__init__.py
+++ preapp/nodes/__init__.py
@@ -2,6 +2,7 @@ from .output_node import OutputNode
 from .platform_node import PlatformNode
 from .framework_node import FrameworkNode
 from .nodejs_node import NodeJSNode
+from .python_node import PythonNode
 from .metadata_node import MetadataNode
 from .github import (
     GithubCredentialsNode,
