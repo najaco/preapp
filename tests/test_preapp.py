fd8173c003306d6d8563146ddea9849f1614a08b
Stephen Davis <davi1138@purdue.edu>
2020-05-23 15:49:06 -0400

    (chore): removed global variable

--- tests/test_preapp.py
+++ tests/test_preapp.py
@@ -6,26 +6,19 @@ from typing import Dict, Any
 import os
 
 
-PROJECT_NAME: str = "test"
-
-
-def _setup_web(web_framework: str, config_file: str) -> None:
-    global PROJECT_NAME
-    PROJECT_NAME = f"{PROJECT_NAME} {web_framework}"
-    
+def _setup_web(project_name: str, web_framework: str, config_file: str) -> None:
     # update the config file to have the correct information
     config_fp: TextIOWrapper = open(config_file, "r")
     config_file_source: str = config_fp.read()
     config_fp.close()
 
     config_file_source = config_file_source.replace("__WEB_FRAMEWORK__", web_framework)
-    config_file_source = config_file_source.replace("__PROJECT_NAME__", PROJECT_NAME)
+    config_file_source = config_file_source.replace("__PROJECT_NAME__", project_name)
 
     new_config_file: str = f"tests/{web_framework}-test-config.json"
     config_fp: TextIOWrapper = open(new_config_file, "w")
     config_fp.write(config_file_source)
     config_fp.close()
-
 
     process = subprocess.Popen(
         f"python -m preapp --preset {new_config_file} --credentials {os.getcwd()}/tests/credentials.json",
@@ -35,13 +28,13 @@ def _setup_web(web_framework: str, config_file: str) -> None:
     stdout, _ = process.communicate()
 
 
-def _teardown_web(github_object: Github) -> None:
+def _teardown_web(project_name: str, github_object: Github) -> None:
     # delete github repo
-    github_object.get_user().get_repo(PROJECT_NAME).delete()
+    github_object.get_user().get_repo(project_name).delete()
 
     # delete local folder
     process = subprocess.Popen(
-        f"rm -rf {os.getcwd()}/{PROJECT_NAME}", shell=True, stdout=subprocess.PIPE,
+        f"rm -rf {os.getcwd()}/{project_name}", shell=True, stdout=subprocess.PIPE,
     )
     stdout, _ = process.communicate()
 
@@ -71,36 +64,36 @@ def name(pytestconfig):
 @pytest.mark.parametrize("name", ["name"])
 @pytest.fixture(scope="function", params=["react", "angular", "vue"])
 def web_framework(name, request):
-    global PROJECT_NAME
-    PROJECT_NAME = name
-    _setup_web(request.param, f"{os.getcwd()}/tests/web-config.json")
-    yield web_framework
-    _teardown_web(get_github_object())
+    project_name: str = f"{name}-{request.param}"
+
+    _setup_web(project_name, request.param, f"{os.getcwd()}/tests/web-config.json")
+    yield project_name
+    _teardown_web(project_name, get_github_object())
 
 
 def test_web(web_framework):
     # check for initial git files
-    assert os.path.isdir(f"{os.getcwd()}/{PROJECT_NAME}")
-    assert os.path.isfile(f"{os.getcwd()}/{PROJECT_NAME}/README.md")
-    assert os.path.isfile(f"{os.getcwd()}/{PROJECT_NAME}/LICENSE")
-    assert os.path.isfile(f"{os.getcwd()}/{PROJECT_NAME}/.gitignore")
+    assert os.path.isdir(f"{os.getcwd()}/{web_framework}")
+    assert os.path.isfile(f"{os.getcwd()}/{web_framework}/README.md")
+    assert os.path.isfile(f"{os.getcwd()}/{web_framework}/LICENSE")
+    assert os.path.isfile(f"{os.getcwd()}/{web_framework}/.gitignore")
 
     # check for website folder
-    assert os.path.isdir(f"{os.getcwd()}/{PROJECT_NAME}/website")
+    assert os.path.isdir(f"{os.getcwd()}/{web_framework}/website")
 
     # check for github actions file
-    assert os.path.isfile(f"{os.getcwd()}/{PROJECT_NAME}/.github/workflows/nodejs.yml")
+    assert os.path.isfile(f"{os.getcwd()}/{web_framework}/.github/workflows/nodejs.yml")
 
     # check for backend
-    assert os.path.isdir(f"{os.getcwd()}/{PROJECT_NAME}/backend")
-    assert os.path.isfile(f"{os.getcwd()}/{PROJECT_NAME}/backend/requirements.txt")
-    assert os.path.isfile(f"{os.getcwd()}/{PROJECT_NAME}/backend/noxfile.py")
-    assert os.path.isfile(f"{os.getcwd()}/{PROJECT_NAME}/backend/setup.py")
+    assert os.path.isdir(f"{os.getcwd()}/{web_framework}/backend")
+    assert os.path.isfile(f"{os.getcwd()}/{web_framework}/backend/requirements.txt")
+    assert os.path.isfile(f"{os.getcwd()}/{web_framework}/backend/noxfile.py")
+    assert os.path.isfile(f"{os.getcwd()}/{web_framework}/backend/setup.py")
 
-    setup_fp: TextIOWrapper = open(f"{os.getcwd()}/{PROJECT_NAME}/backend/setup.py", "r")
+    setup_fp: TextIOWrapper = open(f"{os.getcwd()}/{web_framework}/backend/setup.py", "r")
     setup_source: str = setup_fp.read()
 
-    assert setup_source.find(f'name="{PROJECT_NAME}"') != -1
+    assert setup_source.find(f'name="{web_framework}"') != -1
     assert setup_source.find('description="test"') != -1
     assert setup_source.find('version="0.0.1"') != -1
     assert setup_source.find('email="test"') != -1
@@ -109,13 +102,15 @@ def test_web(web_framework):
 
     setup_fp.close()
 
-    assert os.path.isdir(f"{os.getcwd()}/{PROJECT_NAME}/backend/tests")
-    assert os.path.isfile(f"{os.getcwd()}/{PROJECT_NAME}/backend/tests/test_util.py")
+    assert os.path.isdir(f"{os.getcwd()}/{web_framework}/backend/tests")
+    assert os.path.isfile(f"{os.getcwd()}/{web_framework}/backend/tests/test_util.py")
 
-    assert os.path.isdir(f"{os.getcwd()}/{PROJECT_NAME}/backend/{PROJECT_NAME}")
-    assert os.path.isfile(f"{os.getcwd()}/{PROJECT_NAME}/backend/{PROJECT_NAME}/__init__.py")
-    assert os.path.isfile(f"{os.getcwd()}/{PROJECT_NAME}/backend/{PROJECT_NAME}/__main__.py")
+    assert os.path.isdir(f"{os.getcwd()}/{web_framework}/backend/{web_framework}")
+    assert os.path.isfile(f"{os.getcwd()}/{web_framework}/backend/{web_framework}/__init__.py")
+    assert os.path.isfile(f"{os.getcwd()}/{web_framework}/backend/{web_framework}/__main__.py")
 
     # check for repository
     github_object = get_github_object()
-    assert github_object.get_user().get_repo(PROJECT_NAME).get_commits(sha="master").totalCount == 5
+    assert (
+        github_object.get_user().get_repo(web_framework).get_commits(sha="master").totalCount == 5
+    )
